3f3a86e0249bea72212f6c6d9c5d469caeb605bc
Tixie <contact@tixie.name>
2024-12-30 20:11:07 +0100

    Improve dev navigation's semantic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,28 @@ export function Header() {
 	const { url } = useLocation();
 
 	return (
-		<header>
-			<nav>
-				<a href="/" class={url == '/' && 'active'}>
+		<header class="container">
+			<nav class="flex flex-wrap gap--mini" aria-labelledby="dev-nav">
+				<div id="dev-nav"><span aria-hidden="true">[🚧 </span>dev navigation<span aria-hidden="true">]:</span></div>
+				<a
+					href="/"
+					class={url == '/' && 'text-underline'}
+					aria-current={url == '/' ? 'page' : null}
+				>
 					Home
-				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
+				</a> -
+				<a
+					href="/capture"
+					class={url == '/capture' && 'text-underline'}
+					aria-current={url == '/capture' ? 'page' : null}
+				>
+					Capture
+				</a> -
+				<a
+					href="/404"
+					class={url == '/404' && 'text-underline'}
+					aria-current={url == '/404' ? 'page' : null}
+				>
 					404
 				</a>
 			</nav>
